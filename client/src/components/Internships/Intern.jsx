5f51367bbff682d8b361d85921d8079764df424d
anshumanprajapati2810 <22it003@charusat.edu.in>
2024-05-30 11:04:27 +0530

    Job/Internship pages done (css issue)

--- client/src/components/Internships/Intern.jsx
+++ client/src/components/Internships/Intern.jsx
@@ -3,6 +3,7 @@ import { Link } from 'react-router-dom'
 import { useState, useEffect} from 'react';
 import '../Internships/Intern.css'
 import InternShipData from '../Data/InternshipDatAvl'
+import compLogo from '../../Assets/netflix.png'
 
 function Intern() {
   const [serachCategory,setSearchCategory]=useState("");
@@ -23,12 +24,12 @@ function Intern() {
   const handleCategoryChange=(e)=>{
     const categeoryValue=e.target.value;
     setSearchCategory(categeoryValue);
-    setFilterInternship([categeoryValue,searchLoaction])
+    filterInternship(categeoryValue, searchLoaction)
   }
   const handleCategoryLocationChange=(e)=>{
     const loactionValue=e.target.value;
     setSearchLocation(loactionValue);
-    setFilterInternship([serachCategory,loactionValue])
+    filterInternship(serachCategory,loactionValue)
   }
 
   const filterInterships = (category, location) => {
@@ -40,7 +41,7 @@ function Intern() {
             internship.location.toLowerCase().includes(location.toLowerCase())
         );
         setFilterInternship(filterData);
-        console.log(setFilterInternship.length)
+      
       
     // }
   };
@@ -90,13 +91,93 @@ console.log(filterInternship)
         </div>
       </div>
       <div className="all-internships">
-        <div className=" show show2 flex justify-center">
-          <p className='filterico text-center' onClick={showDiv}>filter <i className="bi bi-funnel  text-blue-400"></i>   </p>
+    <div className=" show show2 flex justify-center">
+      <p className='filterico text-center' onClick={showDiv}>filter <i className="bi bi-funnel  text-blue-400"></i>   </p>
 
-        </div>
-        <p className='head font-bold text-lg text-center '  >{filterInterships.length} total internships</p>
+    </div>
+    <p className='head font-bold text-lg text-center '  >{filterInternship.length} total internships</p>
 
-      </div>
+    { filterInternship.map((data,index)=>(
+
+<div className='shadow-lg rounded-lg bg-white m-7 ' id='display'>
+  <div className="m-4 pt-2">
+  <p className='mb-4 mt-2 ' id='boxer'> <i className='bi bi-arrow-up-right text-blue-500' ></i> Actively Hiring</p>
+  <div className="flex justify-end">
+<img src={compLogo} className='w-14' alt="" />
+  </div>
+<div className="all-ele">
+
+
+<div className='text-lg text-black  mt-7 font-bold'>{data.title}</div>
+<div className="info">
+<p className='text-sm text-slate-300 font-bold'>{data.company}</p>
+<p className=' mt-2'>{data.location}</p>
+</div>
+<div className="flex text-sm justify-between">
+  <p className='mt-3'> <i className="bi bi-play-circle-fill"></i>   Start Date  <br />  {data.StartDate}</p>
+
+
+  <p className='mt-3'> <i className="bi bi-calendar-check-fill"></i>  Duration  <br />
+  {data.Duration}</p>
+
+  <p className='mt-3'>  <i className="bi bi-cash"></i>   Stipend <br /> {data.stipend}</p>
+   </div>
+   </div>
+   <span className='bg-slate-200 text-slate-400 w-20 rounded-sm text-center'>Internship</span>
+   <br />
+   <span><i className="bi bi-stopwatch text-green-300"></i>23/11/2065</span>
+   <div className="flex justify-end" id='hr'>
+<Link to={`/detailInternship?q=${data._id}`} className='mt-10'><button id='viewButtons' className='bg-transparent text-blue-500'>View In Deatils</button></Link>
+   </div>
+  </div>
+  </div>
+  
+    ))
+     
+    }
+
+  </div>
+  {
+  isDivVisible &&(
+    <>
+    <div className="first2-int mb-14">
+  <div className="filter-section w-1/6">
+      <button id='close-btn' onClick={hideDiv}><i className=" text-3xl bi bi-x"></i></button>
+<p className='text-center'><i className="bi bi-funnel  text-blue-400"></i> Filter</p>
+<div className='fill flex flex-col ml-2'>
+<label htmlFor="pro">Profile</label>
+<input type="text" id='pro'  value={serachCategory} onChange={handleCategoryChange} className='profile border-2 mr-3 w-full' placeholder='Profile manager'/>
+<label htmlFor="loc">Location</label>
+<input type="text" id='loc'  value={searchLoaction}  onChange={handleCategoryLocationChange} className='location border-2 mt-10  -ml-8 w-full' placeholder='Mumbai'/>
+</div>
+<div className=" preferences mt-8 flex flex-col">
+<div className="flex mt-3 ml-3 mr-3">
+  <input type="checkbox" name="wfh" id="whf"  className='mr-2 ml-3'/>
+  <label htmlFor="wfh">Work From home</label>
+</div>
+<div className="flex mt-3 ml-3 mr-3">
+<input type="checkbox" name="pt" id="whf"  className='mr-2 ml-3'/>
+  <label htmlFor="pt">Part-time</label>
+</div>
+<p> Annual salary (in lakhs)</p>
+<input type="range" name="" id="" />
+<p className='mt-2 ml-3 mr-3'>0  2K  &nbsp;  4k  &nbsp;  6K &nbsp;  8k   &nbsp; 10K</p>
+</div>
+
+<p className= ' mt-5 text-blue-400'>View more filters <i className="bi bi-chevron-down"></i></p>
+<span className='justify-end flex text-blue-400 mr-3'>Clear all</span>
+</div>
+<div className="search-2"><div className="search-container">
+  <label htmlFor="ex ">Experince</label>
+  <input type="text" id='ex' placeholder='eg. 0-1 year' />
+  <div className="search-icon">
+  <i className="bi bi-search"></i>
+  </div>
+  </div></div>
+  </div>
+    </>
+  )
+}
     </div>
     </>  
     
--- client/src/components/Jobs/JobAvl.jsx
+++ client/src/components/Jobs/JobAvl.jsx
@@ -1,8 +1,186 @@
-import React from 'react'
-
+import React, { useEffect, useState } from 'react'
+import "./Job.css"
+import compLogo from "../../Assets/netflix.png"
+import { Link } from 'react-router-dom';
+import JobData from '../Data/JobDataAvl';
 function JobAvl() {
+
+const [serachCategory,setSearchCategory]=useState("");
+const [searchLoaction,setSearchLocation]=useState("")
+const [jobData,setJobData]=useState([])
+const [filterJob,setFilterJob]=useState([])
+const [isDivVisible,setDivVisible]=useState(false)
+
+  const showDiv=()=>{
+  setDivVisible(true)
+  }
+  const hidediv=()=>{
+    setDivVisible(false)
+  }
+
+  const handleCategoryChange=(e)=>{
+    const categeoryValue=e.target.value;
+    setSearchCategory(categeoryValue);
+    setFilterJob([categeoryValue,searchLoaction])
+  }
+  
+  const handleCategoryLocationChange=(e)=>{
+    const loactionValue=e.target.value;
+    setSearchLocation(loactionValue);
+    setFilterJob([serachCategory,loactionValue])
+  }
+const filterJobs=(category,location)=>{
+  const filterData=JobData.filter(
+    (Job)=>
+    Job.category.toLowerCase().includes(category.toLowerCase())&&
+    Job.location.toLowerCase().includes(location.toLowerCase())
+    )
+    setFilterJob(filterData)
+}
+useEffect(()=>{
+
+  filterJobs(serachCategory,searchLoaction);
+
+},[searchLoaction,serachCategory])
+
+
+ 
   return (
-    <div>JobAvl</div>
+    <>
+    <div className='flex internship-filter'>
+    <div className="first-int  mb-14">
+        <div className="filter-section w-1/5">
+          <p className='text-center' id='filter-ico'><i className='bi bi-funnel text-blue-400'></i>Filter</p>
+          <div className="fill flex flex-col ml-2 mr-2 mt-2">
+            <label htmlFor="pro">Profile</label>
+            <input type="text" value={serachCategory} onChange={handleCategoryChange} className='profile border-2 mr-3 w-full' id='pro' placeholder='Profile Manager'  />
+            <label htmlFor="loc">Location</label>
+           <input type="text" value={searchLoaction} onChange={handleCategoryLocationChange} className='profile border-2 mr-3 w-full' id='loc' placeholder='Ahmedabad'  />
+          </div>
+          <div className="preferences flex flex-col">
+            <div className="flex mt-3  mr-3">
+              <input type="checkbox" id='whf' name='wfh' className='mr-2 ml-3'/>
+              <label htmlFor="wfh">Work From Home</label>  
+            </div>
+            <div className="flex mt-3  mr-3">
+              <input type="checkbox" id='whf' name='pt' className='mr-2 ml-3'/>
+              <label htmlFor="pt">Part-time</label>  
+            </div>
+            <p className='p-1'></p>
+            <input type="range" name='' id='' />
+            <p className="mt-2 ml-3 mr-3">0  &nbsp; 2K &nbsp;  4K  &nbsp;  6K  &nbsp;  8K &nbsp;</p>
+          </div>
+        <p className='mt-5 text-blue-400 text-center'>View More Filters <i className='bi bi-chevron-down'></i></p>
+        <span className='justify-end flex text-blue-400 mr-2 mb-2'>Clear All</span>
+        </div>
+        <div className="search-2">
+          <div className="search-container">
+            <input type="text" placeholder='e.g Design, Mumbai'/>
+            &nbsp;
+            <div className="search-icon">
+              <i className='bi bi-search'></i>
+            </div>
+           </div>
+        </div>
+      </div>
+
+  <div className="all-internships">
+    <div className=" show show2 flex justify-center">
+      <p id='filter-ico' className='filterico text-center' >filter <i class="bi bi-funnel  text-blue-400" onClick={showDiv}></i>   </p>
+
+    </div>
+    <p className='head font-bold text-lg text-center '>{ filterJob.length} total Jobs</p>
+
+    { filterJob.map((data,index)=>(
+
+<div className='shadow-lg rounded-lg bg-white m-7 ' id='display'>
+  <div className="m-4 pt-2">
+  <p className='mb-4 mt-3' id='boxer'> <i className='bi bi-arrow-up-right text-blue-500' ></i> Actively Hiring</p>
+  <div className="flex justify-end">
+<img src={compLogo} className='w-14' alt="" />
+  </div>
+<div className="all-ele">
+
+
+<div className='text-lg text-black  mt-7 font-bold'>{data.title}</div>
+<div className="info">
+
+<p className='text-sm text-slate-300 font-bold'>{data.company}</p>
+<p className=' mt-2'>{data.location}</p>
+</div>
+<div className="flex text-sm justify-between">
+  <p className='mt-3'> <i class="bi bi-play-circle-fill"></i>   Start Date  <br />  {data.StartDate}</p>
+
+
+  <p className='mt-3'> <i class="bi bi-calendar-check-fill"></i>  Experience  <br />
+  {data.Experience}</p>
+
+  <p className='mt-3'>  <i class="bi bi-cash"></i>   Salary <br /> {data.CTC}</p>
+   </div>
+   </div>
+   <span className='bg-slate-200 text-slate-400 w-20 rounded-sm text-center'>Job</span>
+   <br />
+   <span><i class="bi bi-stopwatch text-green-300"></i>23/11/2065</span>
+   <div className="flex justify-end" id='hr'>
+<Link className='mt-10' to={`/detailjob?q=${data._id}`}>
+<button id='viewButtons' className='bg-transparent text-blue-500'>View In Deatils</button></Link>
+   </div>
+  </div>
+  </div>
+  
+    ))
+     
+    }
+
+  </div>
+
+
+ 
+</div>
+{
+  isDivVisible &&(
+    <>
+    <div className="first2-int mb-14">
+  <div className="filter-section w-1/6">
+      <button id='close-btn' onClick={hidediv}><i class=" text-3xl bi bi-x"></i></button>
+<p className='text-center'><i class="bi bi-funnel  text-blue-400"></i> Filter</p>
+<div className='fill flex flex-col ml-2'>
+<label htmlFor="pro">Profile</label>
+<input type="text" id='pro'  value={serachCategory} onChange={handleCategoryChange} className='profile border-2 mr-3 w-full' placeholder='Profile manager'/>
+<label htmlFor="loc">Location</label>
+<input type="text" id='loc'  value={searchLoaction}  onChange={handleCategoryLocationChange} className='location border-2 mt-10  -ml-8 w-full' placeholder='Mumbai'/>
+</div>
+<div className=" preferences mt-8 flex flex-col">
+<div className="flex mt-3 ml-3 mr-3">
+  <input type="checkbox" name="wfh" id="whf"  className='mr-2 ml-3'/>
+  <label htmlFor="wfh">Work From home</label>
+</div>
+<div className="flex mt-3 ml-3 mr-3">
+<input type="checkbox" name="pt" id="whf"  className='mr-2 ml-3'/>
+  <label htmlFor="pt">Part-time</label>
+</div>
+<p> Annual salary (in lakhs)</p>
+<input type="range" name="" id="" />
+<p className='mt-2 ml-3 mr-3'>0  2K  &nbsp;  4k  &nbsp;  6K &nbsp;  8k   &nbsp; 10K</p>
+</div>
+
+<p className= ' mt-5 text-blue-400'>View more filters <i class="bi bi-chevron-down"></i></p>
+<span className='justify-end flex text-blue-400 mr-3'>Clear all</span>
+</div>
+<div className="search-2"><div className="search-container">
+  <label htmlFor="ex ">Experince</label>
+  <input type="text" id='ex' placeholder='eg. 0-1 year' />
+  <div className="search-icon">
+  <i class="bi bi-search"></i>
+  </div>
+  </div></div>
+  </div>
+    </>
+  )
+}
+</>
+
+    
   )
 }
 
