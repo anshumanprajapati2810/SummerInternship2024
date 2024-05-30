7c1bbf67926dac765ad4a033fd8b8e7d6c927e01
anshumanprajapati2810 <22it003@charusat.edu.in>
2024-05-30 23:24:05 +0530

    JobDetails started

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -6,6 +6,8 @@ import Home from './components/Home/Home'
 import Footer from './components/Footer/Footer.jsx'
 import Intern from './components/Internships/Intern.jsx'
 import JobAvl from './components/Jobs/JobAvl.jsx'
+import JobDetail from './components/Jobs/JobDetail.jsx'
+import InternDetail from './components/Internships/InternDetail.jsx'
 function App() {
   const [count, setCount] = useState(0)
 
@@ -18,6 +20,8 @@ function App() {
           {/* <Route path='/register' element={}/> */}
           <Route path='/Internship' element={<Intern/>}/>
           <Route path='/Jobs' element={<JobAvl/>}/>
+          <Route path='/job_details' element={<JobDetail/>}/>
+          <Route path='/intern_details' element={<InternDetail/>}/>
 
         </Routes>
         <Footer/>
--- client/src/components/Jobs/JobDetail.jsx
+++ client/src/components/Jobs/JobDetail.jsx
@@ -1,8 +1,23 @@
 import React from 'react'
-
+import Jobs from '../Data/JobDataAvl'
+import './Job.css'
 function JobDetail() {
   return (
-    <div>JobDetail</div>
+    <div>
+      <div className="details-app">
+        {
+          Jobs.map((data,index)=>(
+            <>
+              <h1 className='font-bold text-3xl text-center'>{data.title}</h1>
+              <div className="m-14 shadow-sm rounded-md border">
+              <p className='mb-4 mt-3' id='boxer'> <i className='bi bi-arrow-up-right text-blue-500' ></i> Actively Hiring</p>
+          
+              </div>
+            </>
+          ))
+        }
+      </div>
+    </div>
   )
 }
 
