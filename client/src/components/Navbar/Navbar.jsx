76192abd1857c1f866ff66641caa779559bb29ff
anshumanprajapati2810 <22it003@charusat.edu.in>
2024-06-05 16:36:29 +0530

    Login button fixed

--- client/src/components/Navbar/Navbar.jsx
+++ client/src/components/Navbar/Navbar.jsx
@@ -29,6 +29,8 @@ function Navbar() {
 
     const showLogin=()=>{
         setDivVisibleForLogin(true)
+        // loginFunction();
+        console.log(user)
     }
     const closeLogin =()=>{
         setDivVisibleForLogin(false)
@@ -92,15 +94,11 @@ function Navbar() {
                         </>
                     ):(
                         <>
-                            <div className="auth">  
-                                    <Link>
-                                        <button className='btn1' onClick={showLogin}>Login</button>
-                                    </Link>
-                                    <Link to={'/register'}>
-                                        <button className='btn2'>Register</button>
-                                    </Link>
+                            <div className="auth">
+                                <button className='btn1' onClick={showLogin}>Login</button>
+                                <button className='btn2'><Link  to="/register">Register</Link></button>
                             </div>
-                        </>
+            </>
                     )
                 }
 
@@ -230,16 +228,16 @@ function Navbar() {
                 <>
                                    <div className="flex bg-white rounded-lg justify-center overflow-hidden mx-auto max-w-sm lg:max-w-4xl">
 <div className="w-full p-8 lg:w-1/2">
-                 <div class="mt-4">
-                 <label class="block text-gray-700 text-sm font-bold mb-2">Email </label>
-                 <input class=" text-gray-700 focus:outline-none focus:shadow-outline border border-gray-300 rounded py-2 px-4 block w-full appearance-none" type="email"  placeholder='john@example.com'/>
+                 <div className="mt-4">
+                 <label className="block text-gray-700 text-sm font-bold mb-2">Email </label>
+                 <input className=" text-gray-700 focus:outline-none focus:shadow-outline border border-gray-300 rounded py-2 px-4 block w-full appearance-none" type="email"  placeholder='john@example.com'/>
              </div>
-             <div class="mt-4">
-                 <div class="flex justify-between">
-                     <label class="block text-gray-700 text-sm font-bold mb-2">Password</label>
-                     <a href="/" class="text-xs text-blue-500">Forget Password?</a>
+             <div className="mt-4">
+                 <div className="flex justify-between">
+                     <label className="block text-gray-700 text-sm font-bold mb-2">Password</label>
+                     <a href="/" className="text-xs text-blue-500">Forget Password?</a>
                  </div>
-                 <input class=" text-gray-700 focus:outline-none focus:shadow-outline border border-gray-300 rounded py-2 px-4 block w-full appearance-none"   placeholder='Must be atleast 6 characters'   type="password"/>
+                 <input className=" text-gray-700 focus:outline-none focus:shadow-outline border border-gray-300 rounded py-2 px-4 block w-full appearance-none"   placeholder='Must be atleast 6 characters'   type="password"/>
              </div>
              <div className="mt-8">
              <button className='btn3  bg-blue-500 h-9 text-white font-bold py-2 px-4 w-full rounded hover:bg-blue-600 '>Login</button>
