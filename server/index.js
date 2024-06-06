098690763476a6d05a9154bba39fd344c64079c7
anshumanprajapati2810 <22it003@charusat.edu.in>
2024-06-06 18:28:48 +0530

    BE : server configured

---
+++ server/index.js
@@ -0,0 +1,20 @@
+const bodyParser = require("body-parser")
+const express  = require("express")
+const app = express()
+const path = require("path")
+const port = process.env.PORT || 3000
+const cors = require("cors")
+
+app.use(cors())
+app.use(bodyParser.json({limit : "50mb"}))
+app.use(bodyParser.urlencoded({extended: true, limit: "50mb"}))
+app.use(express.json())
+
+
+app.get('/', (req,res)=>{
+    res.send("Hello this is my backend")
+})
+
+app.listen(port,()=>{
+    console.log(`Server running on ${port}`)
+})
