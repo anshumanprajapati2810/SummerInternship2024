c0fa66c618ba7c74f5a7fa3965032d4419ed9743
anshumanprajapati2810 <22it003@charusat.edu.in>
2024-06-06 18:38:12 +0530

    BE : cors error resolved

--- server/index.js
+++ server/index.js
@@ -9,7 +9,11 @@ app.use(cors())
 app.use(bodyParser.json({limit : "50mb"}))
 app.use(bodyParser.urlencoded({extended: true, limit: "50mb"}))
 app.use(express.json())
-
+app.use((req,res,next)=>{
+    req.header("Access-Control-Allow-Origin", "*")
+    res.header("Access-Control-Allow-Origin", "*")
+    next();
+})
 
 app.get('/', (req,res)=>{
     res.send("Hello this is my backend")
